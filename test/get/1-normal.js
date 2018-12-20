594b922c4fd79627fae6d8b84c2f3c598bdbc894
marjune <marjune@163.com>
2018-12-20 23:27:25 +0800

    test: add test case for getting non exists properties

--- test/get/1-normal.js
+++ test/get/1-normal.js
@@ -5,4 +5,5 @@ const obj = {a: {b: {c: 100}}};
 assert.deepEqual(get(obj), {a: {b: {c: 100}}});
 assert.deepEqual(get(obj, 'a'), {b: {c: 100}});
 assert.deepEqual(get(obj, 'a', 'b'), {c: 100});
-assert.equal(get(obj, ['a', 'b', 'c']), 100);
+assert.equal(get(obj, 'a', 'b', 'c', 'd'), undefined);
+assert.equal(get(obj, 'a', 'b', 'c', 'd', 'e'), undefined);
