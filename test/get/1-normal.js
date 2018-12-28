6c6905e5ed4889fcd2b7a812ab4ff2edcb671075
marjune <marjune@163.com>
2018-12-28 15:20:43 +0800

    chore: add extra test cases for `get`

--- test/get/1-normal.js
+++ test/get/1-normal.js
@@ -5,5 +5,10 @@ const obj = {a: {b: {c: 100}}};
 assert.deepEqual(get(obj), {a: {b: {c: 100}}});
 assert.deepEqual(get(obj, 'a'), {b: {c: 100}});
 assert.deepEqual(get(obj, 'a', 'b'), {c: 100});
+
+assert.equal(get(obj, 'a', 'b', 'c'), 100);
+assert.equal(get(obj, ['a'], 'b', 'c'), 100);
+assert.equal(get(obj, ['a'], ['b', 'c']), 100);
+
 assert.equal(get(obj, 'a', 'b', 'c', 'd'), undefined);
 assert.equal(get(obj, 'a', 'b', 'c', 'd', 'e'), undefined);
